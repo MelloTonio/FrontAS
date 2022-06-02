b6dc74ff573bbfbf99184517d27bf0a8f0eb456a
joaoduram <69806618+joaoduram@users.noreply.github.com>
2022-06-02 16:15:54 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,30 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import ReactDOM from 'react-dom';
-
-function Button() {
-  return alert('a');
-}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={" "}>INICIAR</button>
-        <br> </br>
-        <button id='botaoMenu'>LOGIN</button>
-        <br> </br>        
-        <button id='botaoMenu'>ADICIONAR CARTA</button>
+        <button className='botao'>INICIAR</button>
+        <button className='botao'>LOGIN</button>
+        <button className='botao'>ADICIONAR CARTA</button>
       </header>
     </div>
   );
 }
 
-ReactDOM.render(
-  <Button />, 
-  document.getElementById('mountNode'),
-);
-
 export default App;
-
-
